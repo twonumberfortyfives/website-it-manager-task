8b313f0f5e0f48469fef4ad9749044a3874ef751
twonumberfortyfives <sma4no.7@gmail.com>
2024-04-19 16:45:53 +0300

    implemented custom login.html and login view plus basic html login page

--- website/views.py
+++ website/views.py
@@ -1,5 +1,21 @@
+from django.contrib.auth import authenticate, login
 from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    pass
+    return render(request, 'website/base.html')
+
+
+def my_login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return render(request, template_name="website/base.html")
+        else:
+            raise Exception('Invalid username or password')
+    else:
+        return render(request, template_name="registration/login.html")

6e143ae001e39ba1cdc726c0db9583bc6624337d
twonumberfortyfives <sma4no.7@gmail.com>
2024-04-25 20:04:44 +0300

    fixed bug on positions_list.html page plus html and css files which are connected with it

--- website/views.py
+++ website/views.py
@@ -75,11 +75,12 @@ class PositionListView(LoginRequiredMixin, generic.ListView):
     form_class = forms.PositionSearchForm
     context_object_name = "position_list"
     template_name = "website/positions_list.html"
-    paginate_by = 5
+    paginate_by = 3
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(PositionListView, self).get_context_data(**kwargs)
         name = self.request.GET.get("name")
+        context["total_positions_count"] = self.get_queryset().count()
         context["search_form"] = forms.PositionSearchForm(
             initial={"name": name}
         )

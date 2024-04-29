581da5b6a8f531d038c0f36ac40797b6c18e5403
twonumberfortyfives <sma4no.7@gmail.com>
2024-04-29 13:46:33 +0300

    added private position tests (get all positions, template usage)

--- website/tests/test_views.py
+++ website/tests/test_views.py
@@ -18,7 +18,10 @@ class PublicWorkerTest(TestCase):
         response = self.client.get(TASK_URL)
         self.assertNotEqual(response.status_code, 200)
 
-    def test_get_page_gives_all_workers_and_template_usage(self):
+
+class PrivateWorkerTest(TestCase):
+    def setUp(self):
+        self.client = Client()
         position = Position.objects.create(
             name="Test Position"
         )
@@ -31,9 +34,44 @@ class PublicWorkerTest(TestCase):
             first_name="first_name",
         )
         self.client.force_login(user)
+
+    def test_get_page_gives_all_workers_and_template_usage(self):
         response = self.client.get(WORKER_URL)
         self.assertEqual(response.status_code, 200)
         all_workers = Worker.objects.all()
         self.assertEqual(response.context["total_workers_count"], all_workers.count())
         self.assertQuerysetEqual(response.context["worker_list"], all_workers)
         self.assertTemplateUsed(response, "website/workers_list.html")
+
+
+class PublicPositionTest(TestCase):
+    def setUp(self) -> None:
+        self.client = Client()
+
+    def test_login_required(self):
+        response = self.client.get(POSITION_URL)
+        self.assertNotEqual(response.status_code, 200)
+
+
+class PrivatePositionTest(TestCase):
+    def setUp(self) -> None:
+        self.client = Client()
+        position = Position.objects.create(
+            name="Test Position"
+        )
+
+        user = Worker.objects.create_user(
+            username="username",
+            email="email@gmail.com",
+            position=position,
+            last_name="last_name",
+            first_name="first_name",
+        )
+        self.client.force_login(user)
+
+    def test_get_all_positions_and_template_usage(self):
+        response = self.client.get(POSITION_URL)
+        all_positions = Position.objects.all().order_by("id")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "website/positions_list.html")
+        self.assertQuerysetEqual(response.context["position_list"], all_positions)

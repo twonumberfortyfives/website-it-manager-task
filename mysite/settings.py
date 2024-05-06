a8c0a74e33e9b577be927d15f5f41b5e02b0efa7
twonumberfortyfives <sma4no.7@gmail.com>
2024-05-06 13:10:02 +0300

    allowed hosts updated

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "it-manager-task.onrender.com"]
 
 # Application definition
 
